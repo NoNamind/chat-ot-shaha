52a614562bb270346259beb4b83aeb361fbd963a
NoNamind <86645835+NoNamind@users.noreply.github.com>
2022-03-20 18:12:41 +0300

    Update index.py

--- index.py
+++ index.py
@@ -65,5 +65,4 @@ async def refresh_msg(nickname, msg_box):
         
         last_idx = len(chat_msgs)
 
-if __name__ == "__main__":
-    start_server(main, debug=True, port=8080, cdn=False)
+start_server(main, debug=True, port=8080, cdn=False)
